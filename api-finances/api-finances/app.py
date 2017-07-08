70d9d21dd01535a560e9455d9b9bba5e1f5f9997
kriswithank <kristopherswann@gmail.com>
2017-07-08 15:44:28 -0500

    GH-7: Add basic infrastructure for finances api

---
+++ api-finances/api-finances/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {
+            'payment_options': [
+                'credit_card',
+                'cash',
+                'gift_card',
+            ],
+        }
+
+
+api.add_resource(HelloWorld, '/')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
