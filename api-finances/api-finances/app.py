05b5e5b000211ccd41d04a07c1d084d934a27392
kriswithank <kristopherswann@gmail.com>
2017-07-09 00:06:18 -0500

    GH-9: Set up a postgres db
    Set up a way for api-finances to communicate with a postgres
    database. Created an end point that will automatically insert
    a new instance of a model into the database. Created a way to
    initalize the database and to drop the tables in the dabase,
    they are as follows.
    
    docker-compose run api-finances flask initdb
    docker-compose run api-finances flask dropdb
    
    TODO: Put this information in the README

--- api-finances/api-finances/app.py
+++ api-finances/api-finances/app.py
@@ -1,22 +1,52 @@
+import click
 from flask import Flask
+from flask.json import jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@database/postgres'
+db = SQLAlchemy(app)
 api = Api(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class HelloWorld(Resource):
     def get(self):
-        return {
-            'payment_options': [
-                'credit_card',
-                'cash',
-                'gift_card',
-            ],
-        }
+        users = User.query.all()
+        new_user_name = 'myuser{0}'.format(str(len(users)))
+        new_user = User(new_user_name, '{0}@gmail.com'.format(new_user_name))
+        db.session.add(new_user)
+        db.session.commit()
+        latest_user = User.query.all()[-1]
+        return jsonify({'username': latest_user.username, 'email': latest_user.email})
 
 
 api.add_resource(HelloWorld, '/')
+
+
+@app.cli.command()
+def initdb():
+    db.create_all()
+    click.echo('Created all tables')
+
+
+@app.cli.command()
+def dropdb():
+    db.drop_all()
+    click.echo('Dropped all tables')
 
 
 if __name__ == '__main__':
