e41e2cac09dd547a8203e094542b28c76ad15cd5
kriswithank <kristopherswann@gmail.com>
2017-07-16 02:29:30 -0500

    Lint file

--- api-finances/api-finances/app.py
+++ api-finances/api-finances/app.py
@@ -1,7 +1,12 @@
+"""
+Contains common components for api-finances
+
+Such as: app, db, api
+"""
 import click
 from flask import Flask
 from flask.json import jsonify
-from flask_restful import Resource, Api
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,6 +16,7 @@ api = Api(app)
 
 
 class User(db.Model):
+    """A temporary class that's being used for testing purposes"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     email = db.Column(db.String(120))
@@ -24,7 +30,9 @@ class User(db.Model):
 
 
 class HelloWorld(Resource):
+    """Another temporary class for testing purposes"""
     def get(self):
+        """A test method which gets the latest user entered"""
         users = User.query.all()
         new_user_name = 'myuser{0}'.format(str(len(users)))
         new_user = User(new_user_name, '{0}@gmail.com'.format(new_user_name))
@@ -39,12 +47,22 @@ api.add_resource(HelloWorld, '/')
 
 @app.cli.command()
 def initdb():
+    """
+    A command to Initalize SQLAlchemy databases
+
+    Run with 'flask initdb'.
+    """
     db.create_all()
     click.echo('Created all tables')
 
 
 @app.cli.command()
 def dropdb():
+    """
+    A command to Drop all tables in the database
+
+    Run with 'flask dropdb'.
+    """
     db.drop_all()
     click.echo('Dropped all tables')
 
