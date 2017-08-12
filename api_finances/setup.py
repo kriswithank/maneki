23bcaed80ffd3b5a3c4fdec7f3d8da6faf53cda7
kriswithank <kristopherswann@gmail.com>
2017-08-12 15:55:53 -0500

    GH-19: Make api-finances pip installable
    Also change name to api_finances.

---
+++ api_finances/setup.py
@@ -0,0 +1,12 @@
+"""Setup file for api_finances python package."""
+from setuptools import setup
+
+setup(
+    name='api_finances',
+    version='0.1',
+    description='An api for handling personal finances',
+    url='https://github.com/kriswithank/maneki',
+    author='Kris Swann',
+    license='MIT',
+    packages=['api_finances'],
+    zip_safe=False)
