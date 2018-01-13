c9117165e49bfaa84d9a1afae7cbdf68a3ef7e84
kriswithank <kristopherswann@gmail.com>
2018-01-13 13:20:11 -0600

    Clean up new_transaction views a little bit

--- api_finances/api_finances/blueprints/new_transaction/views.py
+++ api_finances/api_finances/blueprints/new_transaction/views.py
@@ -15,18 +15,11 @@ new_transaction = Blueprint(
 def init():
     form = forms.NewTransactionForm(request.form)
     retailer_names = [retailer.name for retailer in models.Retailer.query.all()]
-    form.retailer.widget.options = retailer_names
     payment_type_names = [type.name for type in models.PaymentType.query.all()]
-    form.payment_type.widget.options = payment_type_names
+    form.retailer.options = retailer_names
+    form.payment_type.options = payment_type_names
     if request.method == 'POST' and form.validate():
         session['new_transaction_data'] = form.data
-        # {
-        #     'date': form.date.data,
-        #     'total': form.total.data,
-        #     'tax': form.tax.data,
-        #     'payment_type': form.payment_type.data,
-        #     'retailer': form.retailer.data,
-        #     'description': form.description.data}
         return redirect(url_for('add_transaction_confirm'))
     return render_template('new_transaction/init.html', form=form)
 
@@ -35,28 +28,14 @@ def init():
 def add_transaction_update():
     form = forms.NewTransactionForm(request.form)
     retailer_names = [retailer.name for retailer in models.Retailer.query.all()]
-    form.retailer.widget.options = retailer_names
     payment_type_names = [type.name for type in models.PaymentType.query.all()]
-    form.payment_type.widget.options = payment_type_names
+    form.retailer.options = retailer_names
+    form.payment_type.options = payment_type_names
     new_transaction_data = session.get('new_transaction_data', None)
     if request.method == 'GET' and new_transaction_data is not None:
         form = forms.NewTransactionForm(data=new_transaction_data)
-        form.payment_type.data = 'foobar'
-        # form.date.data = new_transaction_data['date']
-        # form.total.data = new_transaction_data['total']
-        # form.tax.data = new_transaction_data['tax']
-        # form.payment_type.data = new_transaction_data['payment_type']
-        # form.retailer.data = new_transaction_data['retailer']
-        # form.description.data = new_transaction_data['description']
     if request.method == 'POST' and form.validate():
         session['new_transaction_data'] = form.data
-        # {
-        #     'date': form.date.data,
-        #     'total': form.total.data,
-        #     'tax': form.tax.data,
-        #     'payment_type': form.payment_type.data,
-        #     'retailer': form.retailer.data,
-        #     'description': form.description.data}
         return redirect(url_for('new_transaction.confirm'))
     return render_template(
         'new_transaction/update.html',
@@ -69,7 +48,7 @@ def add_transaction_confirm():
     # Redirect if new_transaction not in session
     new_transaction_data = session.get('new_transaction_data', None)
     if new_transaction_data is None:
-        return redirect(url_for('add_transaction_new'))
+        return redirect(url_for('add_transaction.new'))
     form = forms.ConfirmTransactionForm(request.form)
     if request.method == 'POST' and form.validate():
         if form.confirm:
