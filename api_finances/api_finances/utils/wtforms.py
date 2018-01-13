fa0c297ca07983b828a00aa820c52e271937fdbb
kriswithank <kristopherswann@gmail.com>
2018-01-13 08:41:17 -0600

    Add wtform ComboBox to utils

---
+++ api_finances/api_finances/utils/wtforms.py
@@ -0,0 +1,33 @@
+import wtforms
+
+class ComboBoxWidget(object):
+
+    def __init__(self, options=[]):
+        self.options = options
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('type', 'text')
+        kwargs.setdefault('value', field.data or '')
+        input_params = dict(kwargs, name=field.name, list=field.name)
+        input = '<input {}>'.format(
+            wtforms.widgets.html_params(**input_params))
+        datalist = '<datalist id="{}">'.format(field.name)
+        for option in self.options:
+            datalist += '\n<option value="{}">'.format(option)
+        datalist += '\n</datalist>'
+
+        return wtforms.widgets.HTMLString('\n'.join([input, datalist]))
+
+class ComboBoxField(wtforms.Field):
+
+    def __init__(self, label='', validators=None, options=[], **kwargs):
+        super().__init__(label, validators, **kwargs)
+        self.options = options
+        self.widget = ComboBoxWidget(options)
+        self.widget.options = options
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = valuelist[0]
+        else:
+            self.data = ''
