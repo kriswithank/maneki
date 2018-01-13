365f498f5541f8f8c9db7118da5fae347427a1d1
kriswithank <kristopherswann@gmail.com>
2018-01-13 15:17:26 -0600

    Update views to work a bit better

--- api_finances/api_finances/blueprints/new_transaction/forms.py
+++ api_finances/api_finances/blueprints/new_transaction/forms.py
@@ -3,15 +3,15 @@ from api_finances.utils.wtforms import ComboBoxField
 
 
 class NewTransactionForm(wtforms.Form):
-    date = wtforms.fields.DateField('Date', format='%Y-%m-%d')
-    total = wtforms.IntegerField('Total')
-    tax = wtforms.IntegerField('Tax')
-    payment_type = ComboBoxField('Payment Type')
-    retailer = ComboBoxField('Retailer')
-    description = wtforms.TextAreaField('Description')
+    date = wtforms.fields.DateField(label='Date', format='%Y-%m-%d')
+    total = wtforms.IntegerField(label='Total')
+    tax = wtforms.IntegerField(label='Tax')
+    payment_type = ComboBoxField(label='Payment Type')
+    retailer = ComboBoxField(label='Retailer')
+    description = wtforms.TextAreaField(label='Description')
     #TODO: Add submit button and clear button
 
 
 class ConfirmTransactionForm(wtforms.Form):
-    confirm = wtforms.SubmitField('Confirm')
-    cancel = wtforms.SubmitField('Cancel')
+    confirm = wtforms.SubmitField(label='Confirm')
+    cancel = wtforms.SubmitField(label='Cancel')
--- api_finances/api_finances/blueprints/new_transaction/views.py
+++ api_finances/api_finances/blueprints/new_transaction/views.py
@@ -1,6 +1,6 @@
 from flask import (Blueprint, render_template, request, session, redirect,
                    url_for)
-from werkzeug.datastructures import ImmutableMultiDict
+from werkzeug.datastructures import MultiDict
 from api_finances import models
 from api_finances.blueprints.new_transaction import forms
 
@@ -13,48 +13,36 @@ new_transaction = Blueprint(
 
 @new_transaction.route('/', methods=['GET', 'POST'])
 def init():
-    form = forms.NewTransactionForm(request.form)
-    retailer_names = [retailer.name for retailer in models.Retailer.query.all()]
-    payment_type_names = [type.name for type in models.PaymentType.query.all()]
-    form.retailer.options = retailer_names
-    form.payment_type.options = payment_type_names
-    if request.method == 'POST' and form.validate():
-        session['new_transaction_data'] = form.data
-        return redirect(url_for('add_transaction_confirm'))
-    return render_template('new_transaction/init.html', form=form)
-
-
-@new_transaction.route('/update', methods=['GET', 'POST'])
-def add_transaction_update():
-    form = forms.NewTransactionForm(request.form)
+    form = forms.NewTransactionForm(data=request.form)
     retailer_names = [retailer.name for retailer in models.Retailer.query.all()]
     payment_type_names = [type.name for type in models.PaymentType.query.all()]
     form.retailer.options = retailer_names
     form.payment_type.options = payment_type_names
     new_transaction_data = session.get('new_transaction_data', None)
     if request.method == 'GET' and new_transaction_data is not None:
-        form = forms.NewTransactionForm(data=new_transaction_data)
+        form = forms.NewTransactionForm(formdata=MultiDict(new_transaction_data))
     if request.method == 'POST' and form.validate():
-        session['new_transaction_data'] = form.data
+        session['new_transaction_data'] = MultiDict(form.data)
         return redirect(url_for('new_transaction.confirm'))
     return render_template(
-        'new_transaction/update.html',
-        new_transaction_data=ImmutableMultiDict(new_transaction_data),
+        'new_transaction/init.html',
+        new_transaction_data=new_transaction_data,
         form=form)
 
 
 @new_transaction.route('/confirm', methods=['GET', 'POST'])
-def add_transaction_confirm():
+def confirm():
     # Redirect if new_transaction not in session
     new_transaction_data = session.get('new_transaction_data', None)
     if new_transaction_data is None:
-        return redirect(url_for('add_transaction.new'))
+        return redirect(url_for('new_transaction.init'))
     form = forms.ConfirmTransactionForm(request.form)
     if request.method == 'POST' and form.validate():
-        if form.confirm:
+        if form.confirm.data:
             return 'Confirm'
-        elif form.cancel:
-            return 'Cancel'
+        elif form.cancel.data:
+            session.pop('new_transaction_data')
+            return 'Cancel, popped from session'
     return render_template(
         'new_transaction/confirm.html',
         new_transaction_data=new_transaction_data,
