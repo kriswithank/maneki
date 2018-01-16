937a1689f3ff8ddcfd8c3b0cdc61ea0073545c35
kriswithank <kristopherswann@gmail.com>
2018-01-15 20:23:16 -0600

    Stop using session for new_transaction forms

--- api_finances/api_finances/blueprints/new_transaction/views.py
+++ api_finances/api_finances/blueprints/new_transaction/views.py
@@ -13,37 +13,28 @@ new_transaction = Blueprint(
 
 @new_transaction.route('/', methods=['GET', 'POST'])
 def init():
-    form = forms.NewTransactionForm(data=request.form)
+    form = forms.NewTransactionForm(request.form)
     retailer_names = [retailer.name for retailer in models.Retailer.query.all()]
     payment_type_names = [type.name for type in models.PaymentType.query.all()]
     form.retailer.options = retailer_names
     form.payment_type.options = payment_type_names
-    new_transaction_data = session.get('new_transaction_data', None)
-    if request.method == 'GET' and new_transaction_data is not None:
-        form = forms.NewTransactionForm(formdata=MultiDict(new_transaction_data))
     if request.method == 'POST' and form.validate():
-        session['new_transaction_data'] = MultiDict(form.data)
         return redirect(url_for('new_transaction.confirm'), 307)
-    return render_template(
-        'new_transaction/init.html',
-        new_transaction_data=new_transaction_data,
-        form=form)
+    return render_template('new_transaction/init.html', form=form)
 
 
 @new_transaction.route('/confirm', methods=['GET', 'POST'])
 def confirm():
-    # Redirect if new_transaction not in session
-    new_transaction_data = session.get('new_transaction_data', None)
-    if new_transaction_data is None:
-        return redirect(url_for('new_transaction.init'))
+    # Redirect if new_transaction can't pass init form
+    temp_init_form = forms.NewTransactionForm(request.form)
+    if not temp_init_form.validate():
+        return redirect(url_for('new_transaction.init'), 307)
     form = forms.ConfirmTransactionForm(request.form)
     if request.method == 'POST' and form.validate():
         if form.confirm.data:
             return 'Confirm'
         elif form.cancel.data:
-            session.pop('new_transaction_data')
             return 'Cancel, popped from session'
     return render_template(
         'new_transaction/confirm.html',
-        new_transaction_data=new_transaction_data,
         form=form)
