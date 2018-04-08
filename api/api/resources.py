a7e949b19dc73b302ed0aefe11c39c2ede8fdeb5
kriswithank <kristopherswann@gmail.com>
2018-04-08 13:42:48 -0500

    Add authentication to api

--- api/api/resources.py
+++ api/api/resources.py
@@ -2,9 +2,10 @@
 from datetime import datetime, timedelta
 
 import jwt
-from flask import current_app
+from flask import current_app, request
 from flask.json import jsonify
 from flask_restful import Resource
+from functools import wraps
 from marshmallow import (Schema, ValidationError, fields, validates,
                          validates_schema)
 from webargs.flaskparser import use_args
@@ -19,28 +20,43 @@ class CredentialSchema(Schema):
         """Enforce required and validated fields by enabling strict evaluation."""
 
         strict = True
+        additional = ('user_id',)
 
     username = fields.Str(required=True)
     password = fields.Str(required=True)
 
-    @validates('username')
-    def user_exists(self, value: str) -> None:
-        """Query the database to see if a user with the given username exists."""
-        target_user = models.User.query.filter_by(username=value).first()
-        if target_user is None:
-            raise ValidationError('No such user exists.')
-
     @validates_schema(skip_on_field_errors=True)
-    def password_is_correct(self, data):
-        """
-        Check that the password is correct for the given user.
-
-        Since this is only run if all field validations pass, we know that target_user
-        is not None.
-        """
+    def validate_user(self, data):
+        """Check that user exists and password is correct."""
         target_user = models.User.query.filter_by(username=data['username']).first()
+        if target_user is None:
+            raise ValidationError('No such user exists.', ['username'])
         if data['password'] != target_user.password:
             raise ValidationError('Password is incorrect.', ['password'])
+        data['user_id'] = target_user.id
+
+
+# class AuthenticationSchema(Schema):
+#     """Schema for authenticating the jwt token provided."""
+
+#     class Meta:
+#         """Enforce required and validated fields by enabling strict evaluation."""
+
+#         strict = True
+#         additional = ('payload', 'user_id')
+
+#     token = fields.Str(required=True)
+
+#     @validates_schema(skip_on_field_errors=True)
+#     def validate_token(self, data):
+#         """Check that the jwt token is valid and store user_id."""
+#         token = data['token']
+#         try:
+#             jwt.decode(token, current_app.config['SECRET_KEY'], algorithm='HS256')
+#         except jwt.ExpiredSignatureError:
+#             raise ValidationError('Token expired', ['token'])
+#         except Exception as e:
+#             raise ValidationError('General exception occured', ['token'])
 
 
 class UserResourse(Resource):
@@ -65,15 +81,17 @@ class TokenResourse(Resource):
     @use_args(CredentialSchema())
     def post(self, args):
         """Get a JWT if the credentials are valid."""
-        expiration = datetime.utcnow() + timedelta(seconds=60)
-        content = {
+        expiration = datetime.utcnow() + timedelta(minutes=15)
+        payload = {
             'exp': expiration,
-            'username': args['username'],
+            'user_id': args['user_id'],
         }
+
+        current_app.logger.warn('token args: ' + str(args))
 
         # Recall that encode returns a bytes object, so we will have to encode it
         # to be able to jsonify the token.
-        token_bytes = jwt.encode(content, current_app.config['SECRET_KEY'], algorithm='HS256')
+        token_bytes = jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm='HS256')
         token = token_bytes.decode('UTF-8')
 
         return jsonify({
@@ -81,9 +99,51 @@ class TokenResourse(Resource):
             'token': token})
 
 
+def authenticate(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Recall that the http Authorization header has the form
+        # Authorization: <type> <credentials/token>
+        auth_header = request.headers.get('Authorization', None)
+
+        if auth_header is None:
+            return jsonify({
+                'result': 'Error',
+                'message': 'Must provide Authorization header'})
+
+        auth_type = auth_header.split(' ')[0]
+        token = auth_header.split(' ')[1]
+
+        if auth_type != 'Bearer':
+            return jsonify({
+                'result': 'Error',
+                'message': 'Must use Authorization type Bearer'})
+
+        payload = None
+        try:
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithm='HS256')
+        except jwt.ExpiredSignatureError:
+            return jsonify({
+                'result': 'Error',
+                'message': 'Token expired'})
+        except Exception as e:
+            return jsonify({
+                'result': 'Error',
+                'message': 'Could not decode jwt'})
+
+        if 'user_id' not in payload:
+            return jsonify({
+                'result': 'Error',
+                'message': 'JWT payload does not contain user id'})
+
+        return func(*args, user_id=payload['user_id'], **kwargs)
+    return wrapper
+
+
 class TransactionResource(Resource):
     """A RESTful resource for transactions belonging to a user."""
 
+    @authenticate
     def get(self, user_id):
         """Return all transactions belonging to the given user."""
         transactions = models.Transaction.query.filter_by(user_id=user_id).all()
@@ -109,4 +169,4 @@ def register_resources(api):
     """Register Flask-RESTful resources to the given api."""
     api.add_resource(UserResourse, '/user')
     api.add_resource(TokenResourse, '/token')
-    api.add_resource(TransactionResource, '/transaction/<int:user_id>')
+    api.add_resource(TransactionResource, '/transaction')
